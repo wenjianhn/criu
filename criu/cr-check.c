bb13328e602cf5f9fcbc32821e970108b3ab937e
Andrei Vagin <avagin@virtuozzo.com>
2017-02-15 02:59:30 +0300

    kerndat: check the SIOCGSKNS ioctl
    This ioctl is called for a socket and returns a file descriptor
    for network namespace where a socket has been created.
    
    Acked-by: Pavel Emelyanov <xemul@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/cr-check.c
+++ criu/cr-check.c
@@ -1062,6 +1062,17 @@ static int check_can_map_vdso(void)
 	return -1;
 }
 
+static int check_sk_netns(void)
+{
+	if (kerndat_socket_netns() < 0)
+		return -1;
+
+	if (!kdat.sk_ns)
+		return -1;
+
+	return 0;
+}
+
 static int (*chk_feature)(void);
 
 /*
@@ -1166,6 +1177,7 @@ int cr_check(void)
 		ret |= check_userns();
 		ret |= check_loginuid();
 		ret |= check_can_map_vdso();
+		ret |= check_sk_netns();
 	}
 
 	/*
@@ -1218,6 +1230,7 @@ static struct feature_list feature_list[] = {
 	{ "uffd", check_uffd },
 	{ "uffd-noncoop", check_uffd_noncoop },
 	{ "can_map_vdso", check_can_map_vdso},
+	{ "sk_ns", check_sk_netns },
 	{ NULL, NULL },
 };
 
--- criu/kerndat.c
+++ criu/kerndat.c
@@ -28,6 +28,7 @@
 #include "proc_parse.h"
 #include "config.h"
 #include "sk-inet.h"
+#include "sockets.h"
 #include <compel/plugins/std/syscall-codes.h>
 #include <compel/compel.h>
 #include "netfilter.h"
@@ -874,6 +875,8 @@ int kerndat_init(void)
 	/* Depends on kerndat_vdso_fill_symtable() */
 	if (!ret)
 		ret = kerndat_vdso_preserves_hint();
+	if (!ret)
+		ret = kerndat_socket_netns();
 
 	kerndat_lsm();
 	kerndat_mmap_min_addr();
