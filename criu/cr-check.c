850c3ef4e284b2f9614676206680599dcd12dffb
Kirill Tkhai <ktkhai@virtuozzo.com>
2017-05-17 15:19:38 +0300

    ns: Add ns_get_parent() feature
    Check for NS_GET_PARENT nsfs ioctl().
    
    Signed-off-by: Kirill Tkhai <ktkhai@virtuozzo.com>
    Signed-off-by: Andrei Vagin <avagin@virtuozzo.com>

--- criu/cr-check.c
+++ criu/cr-check.c
@@ -1095,6 +1095,18 @@ static int check_ns_get_userns(void)
 	return 0;
 }
 
+static int check_ns_get_parent(void)
+{
+	if (kerndat_has_ns_get_parent() < 0)
+		return -1;
+
+	if (!kdat.has_ns_get_parent)
+		return -1;
+
+	return 0;
+}
+
+
 static int (*chk_feature)(void);
 
 /*
@@ -1202,6 +1214,7 @@ int cr_check(void)
 		ret |= check_sk_netns();
 		ret |= check_ns_pid();
 		ret |= check_ns_get_userns();
+		ret |= check_ns_get_parent();
 	}
 
 	/*
@@ -1285,6 +1298,7 @@ static struct feature_list feature_list[] = {
 	{ "link_nsid", check_link_nsid},
 	{ "ns_pid", check_ns_pid},
 	{ "ns_get_userns", check_ns_get_userns },
+	{ "ns_get_parent", check_ns_get_parent },
 	{ NULL, NULL },
 };
 
